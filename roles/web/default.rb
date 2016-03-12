86b54a2d09995ca1c46e1d28339c2ccbb5385d90
Akira Takeguchi <a.takeguchi.sf@gmail.com>
2016-03-12 13:07:18 +0900

    role webのコメントアウト外す

--- roles/web/default.rb
+++ roles/web/default.rb
@@ -1,8 +1,8 @@
-# include_cookbook "base"
-# include_cookbook "yum"
-# include_cookbook "ruby"
-# include_cookbook "mysql"
-# include_cookbook "nginx"
+include_cookbook "base"
+include_cookbook "yum"
+include_cookbook "ruby"
+include_cookbook "mysql"
+include_cookbook "nginx"
 # redis動きません
 # include_cookbook "redis"
 include_cookbook "env"
