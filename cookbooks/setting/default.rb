f5c5ac1939cd2737127757d68a4b2a37172a6e32
Akira Takeguchi <a.takeguchi.sf@gmail.com>
2016-03-25 19:17:08 +0900

    vimrc prompt

---
+++ cookbooks/setting/default.rb
@@ -0,0 +1,33 @@
+USER_NAME = node[:system_user_name]
+HOME = "/home/#{USER_NAME}"
+
+execute "wget git-completion.bash" do
+    user USER_NAME
+    command "wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash; mv ~/git-completion.bash ~/.git-completion.bash"
+end
+
+execute "wget git-prompt.sh" do
+    user USER_NAME
+    command "wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh ~/.git-prompt.sh; mv ~/git-prompt.sh ~/.git-prompt.sh"
+end
+
+BASHRC = "#{HOME}/.bashrc"
+
+remote_file "#{HOME}/bashrc_setting" do
+    source "remote_files/bashrc_setting"
+end
+
+execute "add prompt to .bashrc" do
+    user USER_NAME
+    command "cat ~/bashrc_setting >> #{BASHRC}; rm ~/bashrc_setting"
+end
+
+execute "install neobundle" do
+    user USER_NAME
+    command "mkdir -p #{HOME}/.vim/bundle; git clone https://github.com/Shougo/neobundle.vim #{HOME}/.vim/bundle/neobundle.vim"
+end
+
+remote_file "#{HOME}/.vimrc" do
+    source "remote_files/setting/vim/.vimrc"
+end
+
--- cookbooks/yum/default.rb
+++ cookbooks/yum/default.rb
@@ -1,15 +1,17 @@
-execute "yum clean all" do
-    user "root"
-end
+# execute "yum clean all" do
+#     user "root"
+# end
+#
+# execute "yum update -y" do
+#     user "root"
+# end
 
-execute "yum update -y" do
-    user "root"
-end
-
-package "epel-release"
-package "gcc"
-package "openssl-devel"
-package "libyaml-devel"
-package "readline-devel"
-package "zlib-devel"
+# package "epel-release"
+# package "gcc"
+# package "openssl-devel"
+# package "libyaml-devel"
+# package "readline-devel"
+# package "zlib-devel"
 package "git"
+package "vim"
+package "wget"
